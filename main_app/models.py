b36c2ae98146c1407495b05c27334b1faa2b3a8b
Sung Kim <stwkim103@gmail.com>
2023-06-06 23:48:25 -0400

    create Player model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.urls import reverse
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Player(models.Model):
+    name=models.CharField(max_length=100)
+    species=models.CharField(max_length=100)
+    gender=models.CharField(max_length=100)
+    house=models.CharField(max_length=100)
+    wizard=models.BooleanField()
+    hogwarts_student=models.BooleanField()
+    hogwarts_staff=models.BooleanField()
+    alive=models.BooleanField()
+    image=models.URLField()
+    user=models.ForeignKey(User, on_delete=models.CASCADE)
+    broomsticks = models.ManyToManyField(Broomstick)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'player_id': self.id})
+    
+
+
