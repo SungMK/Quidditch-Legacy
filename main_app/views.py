2583ae9f9f07ff5e283fe362fe81189bc88487d8
Sung Kim <stwkim103@gmail.com>
2023-06-09 01:53:56 -0400

    updates redirect url

--- main_app/views.py
+++ main_app/views.py
@@ -26,7 +26,7 @@ def signup(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('/')
+            return redirect('/teams/create')
         else:
             error_message = 'Invalid Signup - Try Again'
     # GET request
